acd82b42a128d4bf7e6aee79d831b272cb9a337a
Adhari Alabri <151549229+adharialabrii@users.noreply.github.com>
2024-05-02 07:33:37 +0400

    textcontent

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ let countEl = document.getElementById("count-el")
 
 function increment() {
     count += 1
-    countEl.innerText = count //textContent
+    countEl.innerText = count 
 }
 
 function save() {
     let countStr = count + " - "
-    saveEl.innerText+=countStr
-    countEl.innerText = 0
+    saveEl.textContent+=countStr
+    countEl.textContent = 0
     count = 0
 }
 
